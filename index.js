954a0c73179176395058ee31e5c0d49cc55fd1dc
Saswata <saswatamaitra3.14@gmail.com>
2023-06-27 15:21:38 +0530

    Call back functiion and Pyramiid of Doom

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// Example one
 console.log("HTML");
 console.log("CSS");
 console.log("Java Script");
+// Every thing happening one after another
+
+// Example 2
+console.log(" ");
+
+console.log("HTML");
+setTimeout(function(){
+    console.log("CSS");
+},5000)
+console.log("Java Script");
+
+
+const cart =["T-shirt","Laptop","Bag","Mobile"]
+
+api.createOrder(cart, function(){
+    api.proceedPayment(function (){
+        api.showOrderSummary(function(){
+            api.updateWallet()
+        })
+    })
+
+})
+ // The above code is call-back hell  as here we can see Pyramid of DOOM in this code
+ //  Pyramid of doom -   A programming anti-pattern where nested callbacks or if statements create code that is difficult to read, maintain, and understand.
+ 
+
+
+
