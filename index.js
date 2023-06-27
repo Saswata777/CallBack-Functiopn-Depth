4bd6e0dd1a376ea181905ce97b23295292b4c7cc
Saswata <saswatamaitra3.14@gmail.com>
2023-06-27 18:05:54 +0530

    Inversion Control

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ api.createOrder(cart, function(){
 })
  // The above code is call-back hell  as here we can see Pyramid of DOOM in this code
  //  Pyramid of doom -   A programming anti-pattern where nested callbacks or if statements create code that is difficult to read, maintain, and understand.
- 
 
 
 
+// Inversion Controll
+// you loose back your control in codes when you use call Back
